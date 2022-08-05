86747f2d185cbc7cb59b062aa0e9366c178ffc24
svarelalopez <svarelalopez24@gmail.com>
2022-08-05 04:22:04 -0300

    agregué un js

---
+++ js/mostrarlista.js
@@ -0,0 +1,17 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+      
+let listadedatos= listadedatos
+
+for(let elemento of listadedatos) {
+    if (elemento<=listadedatos.length) { 
+        document.getElementById("idlista").innerHTML+=elemento
+
+    }
+}
+
+
+});
+
+
